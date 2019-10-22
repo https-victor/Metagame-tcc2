18dabca04406e913c45e115c79164f66465dd990
Victor Oliveira <jvoliveiralive@gmail.com>
2019-10-22 17:22:56 -0300

    CRUD Games

--- routes/games.js
+++ routes/games.js
@@ -8,35 +8,104 @@ const Game = require('../models/Game');
 // @route   GET api/games
 // @desc    Get all users games
 // @access  Private
-router.get('/', auth, async (req,res) => {
-    try {
-        const games = await Game.find({user: req.user.id}).sort({createdAt: -1});
-        res.json(games);
-    } catch (err) {
-     console.error(err.message);
-     res.status(500).json({errors:[{msg:'Server Error',type:'server'}]});
-    }
+router.get('/', auth, async (req, res) => {
+  try {
+    const games = await Game.find({ user: req.user.id }).sort({
+      createdAt: -1
+    });
+    res.json(games);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ errors: [{ msg: 'Server Error', type: 'server' }] });
+  }
 });
 
 // @route   POST api/games
 // @desc    Add new game
 // @access  Private
-router.post('/', auth, (req,res) => {
-    res.send('Add a new game');
-});
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('name', 'Name is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, description } = req.body;
+
+    try {
+        const newGame = new Game ({
+            name, description, user: req.user.id
+        });
+
+        const game = await newGame.save();
+
+        res.json(game);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({errors:[{msg:'Server Error', type:'server'}]});
+    }
+  }
+);
 
 // @route   PUT api/games/:id
 // @desc    Update game
 // @access  Private
-router.put('/:id', auth, (req,res) => {
-    res.send('Update game');
+router.put('/:id', auth, async (req, res) => {
+    const { name, description } = req.body;
+
+    const gameFields = {};
+    if(name) gameFields.name = name;
+    if(description) gameFields.description = description;
+
+    try {
+        let game = await Game.findById(req.params.id);
+        
+        if(!game) res.status(404).json({errors:[{msg:'Game not found',type:'database'}]});
+
+        // Check if requesting user has permision to the database content
+        if(game.user.toString() !== req.user.id){
+            return res.status(401).json({errors:[{msg:'Not authorized',type:'authorization'}]});
+        }
+
+        game = await Game.findByIdAndUpdate(req.params.id, {$set: gameFields}, {new: true});
+
+        res.json(game);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({errors:[{msg:'Server Error', type:'server'}]});
+    }
 });
 
 // @route   DELETE api/games/:id
 // @desc    Delete game
 // @access  Private
-router.delete('/:id', auth, (req,res) => {
-    res.send('Delete game');
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        let game = await Game.findById(req.params.id);
+        
+        if(!game) res.status(404).json({errors:[{msg:'Game not found',type:'database'}]});
+
+        // Check if requesting user has permision to the database content
+        if(game.user.toString() !== req.user.id){
+            return res.status(401).json({errors:[{msg:'Not authorized',type:'authorization'}]});
+        }
+
+        await Game.findByIdAndRemove(req.params.id);
+
+        res.json({reports:[{msg:'Game deleted', type:'crud'}]});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({errors:[{msg:'Server Error', type:'server'}]});
+    }
 });
 
 module.exports = router;
