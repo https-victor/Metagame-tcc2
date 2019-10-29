d534e43d7d6c9c8c3c0d500af0a26e9d46e9957b
Victor Oliveira <jvoliveiralive@gmail.com>
2019-10-29 13:50:58 -0300

    aaa Fix

--- server.js
+++ server.js
@@ -19,13 +19,13 @@ app.use('/api/games', require('./routes/games'));
 app.use('/api/auth', require('./routes/auth'));
 
 // Serve static assets in production
-
+console.log(process.env.NODE_ENV);
 if(process.env.NODE_ENV === 'production'){
     // Set static folder
 
     app.use(express.static('client/build'));
 
-    app.get('*',(req,res)=>res.sendFile(path.resolve(___dirname,'client/build/index.html')));
+    app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'client/build/index.html')));
 }
 
 const PORT = process.env.PORT || 5000;
