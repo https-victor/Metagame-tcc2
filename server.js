7f115e9d6aae5ae6119bb9f9d3947b92a6d1005e
Victor Oliveira <jvoliveiralive@gmail.com>
2019-10-29 13:19:23 -0300

    Fix do fix

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.use('/api/auth', require('./routes/auth'));
 if(process.env.NODE_ENV === 'production'){
     // Set static folder
 
-    app.use(express.static('/client/build'));
+    app.use(express.static('client/build'));
 
-    app.get('*',(req,res)=>res.sendFile(path.resolve(___dirname,'/client/build/index.html')));
+    app.get('*',(req,res)=>res.sendFile(path.resolve(___dirname,'client/build/index.html')));
 }
 
 const PORT = process.env.PORT || 5000;
